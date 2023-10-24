8c23f4cc7690b3170f87f85a8850f67c375df663
sigibambarbiev <sigibambarbiev@gmail.com>
2023-10-24 21:28:31 +0300

    test2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import ghPages from 'vite-plugin-gh-pages';
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePluginGhPages } from 'vite-plugin-gh-pages';
+
 export default defineConfig({
-  plugins: [react(), ghPages()],
-})
-
-
-
+  plugins: [
+    react(),
+    VitePluginGhPages({
+      build: {
+        // Дополнительные настройки сборки, если необходимо
+      },
+    }),
+  ],
+  // Другие настройки Vite...
+});
