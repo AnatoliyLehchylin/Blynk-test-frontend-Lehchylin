525e8652c112950886eaeb8fa5a4bbca7bdfee03
sigibambarbiev <sigibambarbiev@gmail.com>
2023-10-24 21:41:27 +0300

    otkat

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-//
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import VitePluginGhPages from 'vite-plugin-gh-pages';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginGhPages(),
-  ],
-  // Другие настройки Vite...
+  plugins: [react()],
+})
 
-  build: {
-    target: 'modules',
-    outDir: 'dist',
-  },
-});
 
 
 
